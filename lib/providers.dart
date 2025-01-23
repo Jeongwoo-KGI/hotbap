3a2abfa393af4f9c7a602390c4fedeeaa4f25ba2
seonghoon-02 <tutle02@gmail.com>
2025-01-23 19:00:32 +0900

    refector: icons 폴더 생성. 사용하지 않는 파일 삭제

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hotbap/domain/repository/firebase_repository.dart';
-import 'package:hotbap/domain/usecase/sign_in_with_apple_usecase.dart';
 import 'package:hotbap/pages/login_page/viewmodel/login_viewmodel.dart';
 
 // FirebaseAuth Provider
