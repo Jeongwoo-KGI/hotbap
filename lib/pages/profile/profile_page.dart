fa60e366994bb206211d70b0d29751bec2627df6
ParkChaeEun08 <pce980810@gmail.com>
2025-01-23 15:20:52 +0900

    feat: profile_page

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hotbap/pages/profile/widgets/profile_page_widget.dart';
 
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('프로필'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ProfilePageWidget(),
+      ),
+    );
+  }
+}
