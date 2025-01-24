0d5a4a6203ddd73f7cd3e85ac062acd449be5f8d
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-01-24 20:26:40 +0900

    add: button as a class for the filter

---
+++ lib/filter/widgets/button.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    //white button become red on tap
+    return ;
+  }
+}
