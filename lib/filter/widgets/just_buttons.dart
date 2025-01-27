7991dba17fe95c6e8b600d92ae6837caa7eafc32
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-01-27 16:08:45 +0900

    add: widget without the textbox

---
+++ lib/filter/widgets/just_buttons.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hotbap/filter/widgets/button.dart';
+
+class JustButtons extends StatelessWidget{
+  String input;
+  List<String> content;
+
+  JustButtons({required this.input, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 335,
+          child: Text(
+            "$input",
+            style: TextStyle(
+              color: Color(0xFF333333),
+              fontSize: 20,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w600,
+              height: 1.35,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        SizedBox(
+          height: 29,
+          width: 335,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //for loop widget
+              for (int i = 1; i<content.length; i++)
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: Button(content: content[i]),
+                )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
