427beb67874062cd6eb27144f18c91cd91c1957e
seonghoon-02 <tutle02@gmail.com>
2025-01-27 10:29:13 +0900

    feat: 상세페이지ui 기초 작성,

--- lib/pages/detail_page/detail_page.dart
+++ lib/pages/detail_page/detail_page.dart
@@ -1,16 +1,155 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hotbap/domain/entity/recipe.dart';
 
-class DetailPage extends StatelessWidget {
-  final String rcp_nm; //음식명
+// 상태를 관리하는 StateProvider
+final isHeartFilledProvider = StateProvider<bool>((ref) => false);
 
-  const DetailPage({required this.rcp_nm});
+class DetailPage extends ConsumerWidget {
+  final Recipe recipe; // 음식명
+
+  const DetailPage({required this.recipe, Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isHeartFilled = ref.watch(isHeartFilledProvider); // 현재 상태 값 가져오기
+
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            const SizedBox(
+              // 텍스트 가운데로 하기 위해 추가
+              width: 20,
+            ),
+            const Spacer(),
+            Text(
+              recipe.title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color(0xFF333333),
+                fontSize: 20,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                height: 1.35,
+              ),
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                // 상태 토글 (true ↔ false)
+                ref.read(isHeartFilledProvider.notifier).state = !isHeartFilled;
+              },
+              child: Icon(
+                isHeartFilled
+                    ? CupertinoIcons.heart_fill
+                    : CupertinoIcons.heart, // 상태에 따른 아이콘 변경
+                color: isHeartFilled ? Colors.red : Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
-        child: Text(
-          'DetailPage - $rcp_nm',
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 409,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Flbfit%2FbtsLiTVNW1j%2FC39fMZOruNK3JhuOscrjy0%2Fimg.jpg"),
+                        fit: BoxFit.cover, // contain? cover?
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 69,
+                    height: 26,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFF05937),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(26),
+                      ),
+                    ),
+                    child: const Text(
+                      '국 & 찌개',
+                      style: TextStyle(
+                        color: Color(0xFFFEF7F5),
+                        fontSize: 12,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w600,
+                        height: 1.83,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    recipe.title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color(0xFF333333),
+                      fontSize: 24,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                      height: 1.35,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const SizedBox(
+                    width: 258,
+                    child: Text(
+                      '소금과 간장 대신 북어채와 새우의 짠맛으로 간을 한 담백한 맛의 북엇국을 만들었어요, 홍합이나 바지락을 넣으면 시원한 국물을 연출할 수 있어요.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF999999),
+                        fontSize: 12,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w300,
+                        height: 1.50,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 28),
+                  const Divider(),
+                  const SizedBox(height: 28),
+                  const Text(
+                    '재료',
+                    style: TextStyle(
+                      color: Color(0xFF333333),
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                      height: 1.38,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    '●방울토마토 소박이 : \n방울토마토 150g(5개), 양파 10g(3×1cm), 부추 10g(5줄기)\n●양념장 : \n고춧가루 4g(1작은술), 멸치액젓 3g(2/3작은술), 다진 마늘 2.5g(1/2쪽), 매실액 2g(1/3작은술), 설탕 2g(1/3작은술), 물 2ml(1/3작은술), 통깨 약간',
+                    style: TextStyle(
+                      color: Color(0xFF333333),
+                      fontSize: 14,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w300,
+                      height: 1.50,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
