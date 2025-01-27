7cbc306b0089a2f11cb4a611610ee0a34350e532
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-01-27 17:03:49 +0900

    connect: main page - filter page

--- lib/pages/filter/filter_page.dart
+++ lib/pages/filter/filter_page.dart
@@ -44,6 +44,93 @@ class _FilterPageState extends State<FilterPage> {
             JustButtons(input: "날씨", content: ["날씨좋음", "비가 올 때", "눈이 올 때"]),
             SizedBox(height: 32),
             JustButtons(input: "계절", content: ["봄", "여름","가을","겨울"]),
+
+            //Buttons for Refresh and Search Results
+            SizedBox(height: 70,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              children: [
+                //reset button
+                Container(
+                  width: 110,
+                  height: 56,
+                  padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 14),
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Color(0xFFB3B3B3)),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 24,
+                        height: 24,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(color: Colors.white),
+                        child: Icon(Icons.rotate_left_outlined),
+                      ),
+                      const SizedBox(width: 8),
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "초기화",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xFF333333),
+                                fontSize: 16,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w700,
+                                height: 1.35,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //Search Result Button
+                Container(
+                  width: 218,
+                  height: 56,
+                  padding: const EdgeInsets.symmetric(horizontal: 68, vertical: 14),
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFCCCAc9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '검색결과',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w700,
+                          height: 1.35,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
         ],),
       ),
     );
