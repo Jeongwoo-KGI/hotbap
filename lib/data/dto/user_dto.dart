31ede1cb6851ed9248c133b50ea0ca328c1c9b1e
Jeong woo <jeongwoo@uni.minerva.edu>
2025-01-31 18:07:12 +0900

    fix:UserDto to contain filter elements

--- lib/data/dto/user_dto.dart
+++ lib/data/dto/user_dto.dart
@@ -1,12 +1,18 @@
 class UserDto {
   final String userName;
   final String userId;
+  final String seasonFilter;
+  final String timeFilter;
+  final List<String> ingredientFilter;
 
-  UserDto({required this.userName, required this.userId});
+  UserDto({required this.userName, required this.userId, this.seasonFilter = "", this.ingredientFilter = const ["감자", "돼지고기"], this.timeFilter = ""}); //add ingredientFilter elements when necessary 
 
   Map<String, dynamic> toMap() {
     return {
       'userName': userName,
+      'seasonFilter': seasonFilter,
+      'timeFilter' : timeFilter,
+      'ingredientFilter' : ingredientFilter,
     };
   }
 
@@ -14,6 +20,9 @@ class UserDto {
     return UserDto(
       userName: map['userName'],
       userId: map['userId'],
+      seasonFilter: map['seasonFilter'],
+      timeFilter: map['timeFilter'],
+      ingredientFilter: map['ingredientFilter'],
     );
   }
 }
