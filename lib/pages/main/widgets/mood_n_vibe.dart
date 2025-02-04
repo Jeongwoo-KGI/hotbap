8f478aab9cc56a468e1a78190cbca174f4ec0bd9
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-02-04 17:30:09 +0900

    add: mood_n_vibe.dart finalized

--- lib/pages/main/widgets/mood_n_vibe.dart
+++ lib/pages/main/widgets/mood_n_vibe.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hotbap/domain/entity/recipe.dart';
 import 'package:hotbap/pages/detail_page/detail_page.dart';
 import 'package:hotbap/pages/main/widgets/individual_big.dart';
 
-class MoodNVibe extends StatelessWidget{
+class MoodNVibe extends ConsumerStatefulWidget{
   
   @override
+  _MoodNVibeState createState() => _MoodNVibeState();
+}
+
+class _MoodNVibeState extends ConsumerState<MoodNVibe> {
+  List<Recipe> resultRecipes = [];
+  bool _isLoading = true;
+
+  void initState() {
+    super.initState();
+    moodvibeRecipe();
+  }
+
+  Future<void> moodvibeRecipe() async {
+    List<String> query = ["파스타", "스테이크", "와인", "연인"];
+    List<String> substituteQuery = ['고기', '조기', '파인애플'];
+    List<Recipe> recipes = [];
+    final repository = ref.read(recipeRepositoryProvider);
+    for(int i = 0;i<query.length;i++){
+      recipes += await repository.getRecipesBasedOnGemini(query[i]);
+    }
+    if (recipes.length == 0) {
+      for(int i = 0; i<substituteQuery.length; i++) {
+        recipes += await repository.getJechulRecipeWithoutGemini(substituteQuery[i]);
+      } 
+    }
+    setState(() {
+      resultRecipes = recipes;
+      _isLoading = false;
+    });
+
+  }
+  @override
   Widget build(BuildContext context) {
+    if(_isLoading) {
+      return Center(child: CircularProgressIndicator(),);
+    }
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -25,10 +62,10 @@ class MoodNVibe extends StatelessWidget{
         Container(
           height: 256,
           child: ListView.builder(
-            itemCount: recipeData.length,
+            itemCount: resultRecipes.length,
             itemBuilder: (context, index){
-              final recipe = recipeData[index].data() as Map<String, dynamic>;
-              final input = Recipe.fromMap(recipe);
+              final recipe = resultRecipes[index];
+              final input = recipe;
               return GestureDetector(
                 child: individualBigRecipe(input),
                 onTap: (){
