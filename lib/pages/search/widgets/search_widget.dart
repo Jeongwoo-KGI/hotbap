8734f19b8813a975ffded7a4f2612521dbb1be89
dolbuda13 <dolbuda24@gmail.com>
2025-02-04 16:38:14 +0900

    fix: search page ui 수정, 아이콘 대체

--- lib/pages/search/widgets/search_widget.dart
+++ lib/pages/search/widgets/search_widget.dart
@@ -31,20 +31,35 @@ class _SearchWidgetState extends State<SearchWidget> {
               padding: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 color: _isSearchComplete ? Color(0xFFF2F2F2) : Colors.white,
-                border: Border.all(color: Color(0xFFCCCCCC)),
+                border: Border.all(
+                  color: _isSearchComplete ? Color(0xFFF2F2F2) : Color(0xFFCCCCCC),
+                ),
                 borderRadius: BorderRadius.circular(12),
               ),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(Icons.search, color: Color(0xFFB3B3B3)),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          _isSearchComplete
+                              ? 'assets/icons/search_icon_grey.png'
+                              : 'assets/icons/search_icon_white.png',
+                        ),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
                   SizedBox(width: 8),
                   Expanded(
                     child: TextField(
                       controller: _controller,
                       decoration: InputDecoration(
                         border: InputBorder.none,
-                        hintText: '검색',
+                        hintText: '재료,레시피,키워드를 검색해주세요.',
                         hintStyle: TextStyle(
                           color: Color(0xFFB3B3B3),
                           fontSize: 14,
@@ -97,8 +112,17 @@ class _SearchWidgetState extends State<SearchWidget> {
               ),
             ),
           ),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            width: _showCancelButton ? 12 : 0, // 패딩을 애니메이션 효과로 제거
+          ),
           if (_showCancelButton)
             TextButton(
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: Size(0, 0),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
               onPressed: () {
                 setState(() {
                   _controller.clear();
