9e7307a9ed4aaae785af803088c374e595699411
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-02-10 15:14:47 +0900

    add: guest page

---
+++ lib/pages/main/guest_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hotbap/domain/entity/recipe.dart';
+import 'package:hotbap/pages/main/widgets/jechul_food_rec.dart';
+import 'package:hotbap/pages/main/widgets/logo_and_filter.dart';
+import 'package:hotbap/pages/main/widgets/mood_n_vibe.dart';
+import 'package:hotbap/pages/main/widgets/my_favorites.dart';
+import 'package:hotbap/pages/main/widgets/recipe_result.dart';
+import 'package:hotbap/theme.dart';
+
+class GuestPage extends StatelessWidget{
+  List<Recipe> resultRecipesAI;
+  List<Recipe> resultRecipesMNV;
+  List<Recipe> resultJechul;
+
+  GuestPage({super.key, required this.resultRecipesAI, required this.resultRecipesMNV, required this.resultJechul});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0),
+        child: AppBar(
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //logo and filter button
+            LogoAndFilter(),
+            Padding(
+              padding: EdgeInsets.only(left: 22, bottom: 12, top: 25.73),
+              // child: SayHi(userName: userName)
+            ),
+            //Recipe Results
+            RecipeResult(searchResult: resultRecipesAI),
+            //Recipe Curated1: mood n vibe
+            MoodNVibe(resultRecipes: resultRecipesMNV),
+            //Recipe Jechul
+            JechulFoodRec(resultRecipes: resultJechul,),
+          ],
+        ),
+      ),
+      bottomNavigationBar:
+          BottomNavBar(initialIndex: 0), // 초기 인덱스를 설정하여 네비게이션 바 추가
+      );;
+  }
+}
