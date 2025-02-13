bc06912fb573b62b6549c027a9f5b0dd378191d4
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2025-02-13 20:45:46 +0900

    add: call firebase data with random

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -21,9 +22,10 @@ import 'package:hotbap/pages/main/widgets/say_hi.dart';
  * and page view of the recipies that are customized and tailored for daily usage
  */
 
+//uncomment to revive the save recipe mode (uncomment in the container page needed as well) <- checked with comment 'savemode'
 class MainPage extends ConsumerStatefulWidget {
-  final AiStackUsecase useCase;
-  const MainPage(this.useCase, {super.key});
+  //final AiStackUsecase useCase; //savemode
+  const MainPage({super.key});//,this.useCase); //savemode
 
   @override
   _MainPageState createState() => _MainPageState();
@@ -33,11 +35,11 @@ class _MainPageState extends ConsumerState<MainPage> {
   //initial values
   String userName = '';
   List<String> savedRecipes = [];
+  List<Recipe> resultRecipesMNV =[];
+  List<Recipe> resultRecipesAI =[];
+  List<Recipe> resultJechul=[];
   User? user = FirebaseAuth.instance.currentUser;
   bool isLoading = true;
-  List<Recipe> resultRecipesMNV = [];
-  List<Recipe> resultRecipesAI = [];
-  List<Recipe> resultJechul = [];
 
   @override
   void initState() {
@@ -45,9 +47,10 @@ class _MainPageState extends ConsumerState<MainPage> {
     _initializeData();
   }
 
-  void saveFetchedRecipe(Recipe recipe) {
-    widget.useCase.executeAdd(recipe);
-  }
+  //savemode
+  // void saveFetchedRecipe(Recipe recipe) {
+  //   widget.useCase.executeAdd(recipe);
+  // }
 
   //FixMe: separate these state controller to viewModel
   Future<void> _initializeData() async {
@@ -60,93 +63,109 @@ class _MainPageState extends ConsumerState<MainPage> {
   void dispose() {
     super.dispose();
   }
-
+  
   Future<void> dataRecipeGetAll(User? user) async {
     isLoading = true;
     //for mood and vibe
     // List<String> query = ["파스타","스테이크","와인","연인"];
-    List<String> substituteQuery = [
-      // '파인애플',
-      // '잡채',
-      // '잣',
-      // '자몽',
-      // '복숭아',
-      // '당근',
-      // '귤',
-      // '한라봉',
-      // '미역',
-      // '다시마',
-      // '궁채',
-      // '들깨',
-      // '닭',
-      // '라이스페이퍼',
-      // '청국장',
-      // '다슬기',
-      // '된장',
-      // '고추장',
-      // '수박',
-      '돼지고기',
-      '감자',
-      '돼지감자',
-      '파스타',
-      '국수',
-    ];
+    //for jechul queries
+    // List<String> substituteQuery = [
+    //   '파인애플',
+    //   '잡채',
+    //   '잣',
+    //   '자몽',
+    //   '복숭아',
+    //   '당근',
+    //   '귤',
+    //   // '한라봉',
+    //   // '미역',
+    //   // '다시마',
+    //   // '궁채',
+    //   // '들깨',
+    //   // '닭',
+    //   // '라이스페이퍼',
+    //   // '청국장',
+    //   // '다슬기',
+    //   // '된장',
+    //   // '고추장',
+    //   // '수박',
+    //   // '돼지고기',
+    //   // '감자',
+    //   // '돼지감자',
+    //   // '파스타',
+    //   // '국수',
+    // ];
     List<Recipe> recipes = [];
 
     final repository = ref.read(recipeRepositoryProvider);
 
-    int i = 0;
-    //잠수함패치
-    while (recipes.length < 11 && i < substituteQuery.length) {
-      //random -> indx increment
-      i++;
-      recipes +=
-          await repository.getJechulRecipeWithoutGemini(substituteQuery[i]);
-    }
+    //query directly & save recipe mode
+    // int i = 0;
+    // //잠수함패치
+    // while (recipes.length < 11 && i < substituteQuery.length) {
+    //   //random -> indx increment
+    //   i++;
+    //   recipes +=
+    //       await repository.getJechulRecipeWithoutGemini(substituteQuery[i]);
+    // }
 
-    for (int i = 0; i <recipes.length ; i++) {
-      saveFetchedRecipe(recipes[i]);
-    }
+    // for (int i = 0; i <recipes.length ; i++) {
+    //   saveFetchedRecipe(recipes[i]);
+    // }
 
-    //Saved Recipe
-    QuerySnapshot? recipesSnapshot;
-    user != null
-        ? recipesSnapshot = await getUserData(user.uid)
-        : recipesSnapshot = null;
+    //Call firebase recipe DB
+    QuerySnapshot jechulSnapshot = await getJechulData();
+    final jechulList = jechulSnapshot.docs;
+    for (int i = 0; i<jechulList.length;i++) {
+      final recipe = jechulList[i].data() as Map<String,dynamic>;
+      recipes.add(Recipe.fromMap(recipe));
+    }
+    
+
+    // //Saved favorite Recipe
+    // QuerySnapshot? recipesSnapshot;
+    // user != null
+    //     ? recipesSnapshot = await getUserData(user.uid)
+    //     : recipesSnapshot = null;
 
     //save the data that has been fetched
     if (mounted) {
       //flutter bool var false = disposed screen
       setState(() {
-        if (user != null && recipesSnapshot != null) {
-          savedRecipes = recipesSnapshot.docs
-              .map((doc) => doc['title'] as String)
-              .toList();
-        }
-        resultRecipesMNV = [recipes[0], recipes[1], recipes[2], recipes[3]];
-        resultRecipesAI = [
-          recipes[4],
-          recipes[5],
-          recipes[6],
-          recipes[7],
-          recipes[12]
-        ];
-        resultJechul = [recipes[8], recipes[9], recipes[10], recipes[11]];
+        
+        // if (user != null && recipesSnapshot != null) {
+        //   savedRecipes = recipesSnapshot.docs
+        //       .map((doc) => doc['title'] as String)
+        //       .toList();
+        // }
+        
+        //query recipe directly mode
+        resultRecipesMNV = recipes.sample(5);
+        resultRecipesAI = recipes.sample(6);
+        resultJechul = recipes.sample(4);
         isLoading = false;
-
-
       });
     }
   }
-  //get user data (name & saved recipe)
-  Future<QuerySnapshot> getUserData(String uid) async {
-    QuerySnapshot savedRecipesSnapshot = await FirebaseFirestore.instance
-        .collection('user')
-        .doc(uid)
-        .collection('favorites')
-        .get();
+  // //get user data (name & saved recipe)
+  // Future<QuerySnapshot> getUserData(String uid) async {
+  //   QuerySnapshot savedRecipesSnapshot = await FirebaseFirestore.instance
+  //       .collection('user')
+  //       .doc(uid)
+  //       .collection('favorites')
+  //       .get();
 
-    return savedRecipesSnapshot;
+  //   return savedRecipesSnapshot;
+  // }
+
+  Future<QuerySnapshot> getJechulData() async {
+    QuerySnapshot jechulRecipesSnapshot = await FirebaseFirestore.instance
+      .collection('Suggestion')
+      .doc('fetchedData')
+      .collection('jechul')
+      .get();
+
+    return jechulRecipesSnapshot;
   }
 
   @override
